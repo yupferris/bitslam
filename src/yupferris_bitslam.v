96036221e643441a66c1f22024fe2be651af1847
ferris <yupferris@gmail.com>
2022-11-09 22:39:08 +0100

    Initial test commit
    Haven't set up proper testing locally, but I expect the gh actions setup to do something somewhat-useful here.

--- src/yupferris_bitslam.v
+++ src/yupferris_bitslam.v
@@ -1,18 +1,18 @@
 `default_nettype none
 
-module seven_segment_seconds #( parameter MAX_COUNT = 1000 ) (
-  input [7:0] io_in,
-  output [7:0] io_out
+module yupferris_bitslam(
+    input [7:0] io_in,
+    output [7:0] io_out
 );
-    
+
     wire clk = io_in[0];
     wire reset = io_in[1];
-    wire [6:0] led_out;
-    assign io_out[6:0] = led_out;
 
     // external clock is 1000Hz, so need 10 bit counter
     reg [9:0] second_counter;
     reg [3:0] digit;
+
+    assign io_out[7:0] = second_counter[7:0];
 
     always @(posedge clk) begin
         // if reset, set counter to 0
@@ -37,8 +37,5 @@ module seven_segment_seconds #( parameter MAX_COUNT = 1000 ) (
                 second_counter <= second_counter + 1'b1;
         end
     end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
 
 endmodule
