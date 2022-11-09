780547db71d902d9f96a186eb57f6b0e06538c86
ferris <yupferris@gmail.com>
2022-11-09 22:47:05 +0100

    Clear out more stuff

--- src/yupferris_bitslam.v
+++ src/yupferris_bitslam.v
@@ -8,33 +8,16 @@ module yupferris_bitslam(
     wire clk = io_in[0];
     wire reset = io_in[1];
 
-    // external clock is 1000Hz, so need 10 bit counter
-    reg [9:0] second_counter;
-    reg [3:0] digit;
+    reg [7:0] counter;
 
-    assign io_out[7:0] = second_counter[7:0];
+    assign io_out = counter;
 
     always @(posedge clk) begin
-        // if reset, set counter to 0
         if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == MAX_COUNT) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
+            counter <= 0;
+        end
+        else begin
+            counter <= counter + 1'b1;
         end
     end
 
