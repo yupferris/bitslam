f9edf66c197447eab20cf8bb88ea88ca18f45d02
ferris <yupferris@gmail.com>
2022-11-10 22:26:47 +0100

    Tap mask perhaps

--- src/yupferris_bitslam.v
+++ src/yupferris_bitslam.v
@@ -39,7 +39,17 @@ module yupferris_bitslam(
         end
     end
 
+    reg [2:0] lfsr_tap_mask;
+
+    always @(posedge clk) begin
+        if (write_data && addr == 5'h01)
+            lfsr_tap_mask <= data[2:0];
+    end
+
     reg [7:0] lfsr;
+    wire tap3 = lfsr[3] & lfsr_tap_mask[0];
+    wire tap6 = lfsr[6] & lfsr_tap_mask[1];
+    wire tap7 = lfsr[7] & lfsr_tap_mask[2];
 
     always @(posedge clk) begin
         if (tick) begin
@@ -47,7 +57,7 @@ module yupferris_bitslam(
                 lfsr <= 8'h01;
             end
             else begin
-                lfsr <= {lfsr[6:0], lfsr[3] ^ lfsr[6] ^ lfsr[7]};
+                lfsr <= {lfsr[6:0], tap3 ^ tap6 ^ tap7};
             end
         end
     end
